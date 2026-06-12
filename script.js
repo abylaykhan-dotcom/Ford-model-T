3d1263646bcbe8c62213d97c5050d4c032ce0063
student <student@skillset.edu.kz>
2026-06-12 17:41:11 +0500

    final project

--- script.js
+++ script.js
@@ -10,4 +10,17 @@ button.addEventListener("click", () => {
     aby.style.color = "#2563EB";
     aby.style.fontWeight = "700";
 
-})
+});
+let Forma = document.querySelector("#contactForma");
+let FormaMessage = document.querySelector("#FormaMessage");
+
+Forma.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    FormaMessage.textContent = "Forma submitted successfully!";
+    FormaMessage.style.color = "#00ff40";
+
+    Forma.reset();
+}
+);
+
